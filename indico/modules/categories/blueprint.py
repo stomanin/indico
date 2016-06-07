c4b440a2c1df8dac70583f8bfceb6b0f14eb09e0
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2016-06-07 16:22:58 +0200

    Category picker: Navigate categories

--- indico/modules/categories/blueprint.py
+++ indico/modules/categories/blueprint.py
@@ -17,7 +17,7 @@
 from __future__ import unicode_literals
 
 from indico.modules.categories.controllers.display import (RHCategoryStatistics, RHCategoryIcon, RHCategoryLogo,
-                                                           RHCategoryLoadSubcategories)
+                                                           RHCategoryInfo)
 from indico.modules.categories.controllers.management import (RHManageCategoryContent, RHManageCategoryProtection,
                                                               RHManageCategorySettings, RHManageCategoryIcon,
                                                               RHManageCategoryLogo, RHSortSubcategories, 
@@ -41,4 +41,4 @@ _bp.add_url_rule('/manage/sort-subcategories', 'sort_subcategories', RHSortSubca
 _bp.add_url_rule('/icon-<slug>.png', 'display_icon', RHCategoryIcon)
 _bp.add_url_rule('/logo-<slug>.png', 'display_logo', RHCategoryLogo)
 _bp.add_url_rule('/statistics', 'statistics', RHCategoryStatistics)
-_bp.add_url_rule('/load-subcategories', 'load-subcategories', RHCategoryLoadSubcategories, methods=('POST',))
+_bp.add_url_rule('/info', 'info', RHCategoryInfo)
